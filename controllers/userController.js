2f72615d5a4eea237c81e2c6cb1eb95d9d522bfa
BakayAlexander <bakay.dvr@gmail.com>
2022-03-23 20:26:02 +0300

    fix: fix user controller validation error

--- controllers/userController.js
+++ controllers/userController.js
@@ -59,9 +59,9 @@ exports.updateUserProfile = async (req, res) => {
       });
     }
   } catch (err) {
-    if (err.name === 'CastError') {
+    if (err.name === 'ValidationError') {
       res.status(400).send({
-        message: 'Невалидный id',
+        message: 'Данные невалидны',
       });
     } else {
       res.status(500).send({ message: 'На сервере произошла ошибка.' });
